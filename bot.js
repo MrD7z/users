1f2d1b0effc9e2a3d62b07a3a8d5e1768b766fae
MrD7z <39459185+MrD7z@users.noreply.github.com>
2018-08-21 18:32:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,37 @@ const client = new Discord.Client();
 
 
 
-client.on("guildMemberAdd", member => {
+const text =
+[
+' ** حرام واحد جميل مثلك م يكون عندنا .. تنورنا  **\n https://discord.gg/bzjuptE',
+' ** تسمح لنا نخطفك شوي :hearts: :innocent: **\n https://discord.gg/bzjuptE',
+'مكان راقي ندعوك له :fire::sparkles: \n لنجتمع لأضافة الابداع والجمال للديسكورد \n\n https://discord.gg/bzjuptE',
+'حياكم الله في سيرفر **Coffe** \nسيرفر صديق وجميل بأهله وناسه \nآرواح امتزجت بالطيب والأخوهه \nتصافت قلوبهم على المحبه والمودهه \nيرحبون بالجميع :cherry_blossom: \n\n[ https://discord.gg/bzjuptE ] \nتنورنا 🤭:yellow_heart:.',
+]
+
+    client.on('guildMemberRemove', member => {
     member.createDM().then(function (author) {
-return author.send(' ** تسمح لنا نخطفك شوي :hearts: :innocent: ** \n https://discord.gg/bzjuptE' )
-        
+        return author.send(`${text[Math.floor(Math.random() * text.length)]}`)
+  
   }).catch(console.error)
   })
   
+client.on('guildMemberRemove', member => {
+    member.createDM().then(function (author) {
+    return author.send('** حرام واحد جميل مثلك م يكون عندنا .. تنورنا  **\n https://discord.gg/bzjuptE' )
+  
+  }).catch(console.error)
+  })
+
+
+ client.on('message', message => {
+    if(message.content === 'heyz123') {
+            const voiceChannel = message.member.voiceChannel;
+            if (!voiceChannel) {
+      return ;
+    }
+        voiceChannel.join()
+    }
+})
+
   client.login(process.env.TOKEN);
